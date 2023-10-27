8f5b50ddfe91eaa37e3adb92e3079952cf3f4233
arnitaxioba <xiobazhynonot@gmail.com>
2023-10-27 21:31:01 +0700

    tugas pertemuan 6

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -13,18 +13,18 @@ class Web extends CI_Controller
     public function index()
     {
 
-        $data['judul'] = "Halaman Depan";
-        $this->load->view('v_header',$data);
-        $this->load->view('v_index',$data);
-        $this->load->view('v_footer',$data);
+          $data['judul'] = "Halaman Depan";
+          $this->load->view('v_header',$data);
+          $this->load->view('v_index',$data);
+          $this->load->view('v_footer',$data);
     }
     public function about()
     {
 
-      $data['judul'] = "Halaman About";
-      $this->load->view('v_header', $data);
-      $this->load->view('v_about', $data);
-      $this->load->view('v_footer', $data);
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
     }
           
 }
